ddb6763a11e683a66275e862d883b8e95b786a72
Rinat <netkeyok@gmail.com>
2024-04-19 11:32:31 +0500

    fix dockerfile

--- dbcon/model.py
+++ dbcon/model.py
@@ -1,5 +1,5 @@
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, DateTime, Boolean, SmallInteger, ForeignKey, func
+from sqlalchemy import Column, Integer, String, DateTime, Boolean, SmallInteger, ForeignKey
 from sqlalchemy.orm import relationship
 
 Base = declarative_base()
