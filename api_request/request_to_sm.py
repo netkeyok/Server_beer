f919fe1c76845712d2ff7babaef6500263c0150c
Rinat <netkeyok@gmail.com>
2024-04-29 19:28:44 +0500

    fix functions.py правка request_docs для получения документов даже с неизвестными товарами

--- api_request/request_to_sm.py
+++ api_request/request_to_sm.py
@@ -5,16 +5,18 @@ def get_article_name(gs1):
     barcode = gs1[3:16]
     url = f"http://192.168.0.239:8083/v1/get_card?bar={barcode}"
     response = requests.get(url)
+    # print(response.text)
     data = response.json()
     if data:
         # Предполагаем, что в ответе всегда будет хотя бы один элемент
-        return data[0]['name']
+        return data['name']
     else:
         return None
 
 
 if __name__ == '__main__':
     # Пример вызова функции
-    barcode = '4640122541638'
+    barcode = '0104680089950661215N3fCGj93yMdb'
+    # barcode = '0104680089950555215trj5Ij936FG8'
     name = get_article_name(barcode)
     print(f" Name: {name}")
